2e54b3b8c1beda56359dfa15a5848cc1d396d0d6
LaTherapist <arztpsychiater@gmail.com>
2021-03-24 12:09:06 +0200

    feat: add BG-changing button

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const TIME = document.querySelector('.time'),
   DATE = document.querySelector('.date'),
   FOCUS = document.querySelector('.focus'),
   NAME = document.querySelector('.name'),
+  NEXT = document.querySelector('.next'),
   greeting = document.querySelector('.greeting');
 
+// Return Hours
+const getHour = new Date().getHours(); 
 // Add Time
 const addZero = n => {
     return ( parseInt(n, 10) < 10 ? '0' : '') + n;
@@ -29,14 +32,13 @@ const showDate = () => {
     DATE.innerHTML = `${format}`;
 };
 // Add Greeting
-const dayTime = () => {
-    let hour = new Date().getHours();
+const dayTime = hour => {
     return hour < 6  ? 'night' :
            hour < 12 ? 'morning' :
            hour < 18 ? 'afternoon' : 'evening';
 }
 const showGreeting = () => {
-    greeting.textContent = `Good ${dayTime()}, `;
+    greeting.textContent = `Good ${dayTime(getHour)}, `;
 };
 // Add Name
 const clearField = e => {
@@ -66,18 +68,21 @@ NAME.addEventListener('keydown', enterBlur);
 // Pick a random num
 const random = (min, max) => {
     return Math.floor(min + Math.random() * (max + 1 - min));
-}
+};
 // Change BG
 let randomArr = Array.from({length: 24}, () => random(1, 20));
 
-const changeBG = () => {
-    let today = new Date()
-    let hour = today.getHours();
+const changeBG = hour => {
+    let today = new Date();
+
+    hour = hour % 24;
+    let timeOfDay = dayTime(hour);
+    if (hour >= 12 && hour < 18) timeOfDay = 'day';
 
     let img = new Image()
         base = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/',
         number = addZero(randomArr[hour]),
-        source = `${base}${dayTime()}/${number}.jpg`;
+        source = `${base}${timeOfDay}/${number}.jpg`;
     
     img.src = source;
     img.onload = () => {
@@ -88,11 +93,19 @@ const changeBG = () => {
     let sec = today.getSeconds() * 1000;
     setTimeout(changeBG, 60*60*1000 - min - sec);
 };
+// Button BG-changer 
+let counter = 1;
+
+const nextBG = () => {
+    changeBG( getHour + counter);
+    counter++;
+}
+NEXT.addEventListener('click', nextBG);
 
 document.addEventListener('DOMContentLoaded', () => {
     showTime();
     showDate();
     showGreeting();
     getName();
-    changeBG();
+    changeBG(getHour);
 });
